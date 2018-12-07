752df70c385e450867aff101599c863a2e6c1641
Lucas Lobo <lucas5798fell@gmail.com>
2018-12-07 14:52:10 +0000

    RL.py

--- proj2/RL.py
+++ proj2/RL.py
@@ -1,3 +1,5 @@
+#Grupo 87 | David Sousa Batista 86405 | Lucas Lobo Fell 86464
+
 # -*- coding: utf-8 -*-
 """
 Created on Mon Oct 16 20:31:54 2017
@@ -6,10 +8,11 @@ Created on Mon Oct 16 20:31:54 2017
 """
 import numpy as np
 import random
+import copy
 
 from tempfile import TemporaryFile
 outfile = TemporaryFile()
-	
+
 class finiteMDP:
 
     def __init__(self, nS, nA, gamma, P=[], R=[], absorv=[]):
@@ -20,9 +23,7 @@ class finiteMDP:
         self.P = P
         self.R = R
         self.absorv = absorv
-        # completar se necessario
-        
-            
+
     def runPolicy(self, n, x0,  poltype = 'greedy', polpar=[]):
         #nao alterar
         traj = np.zeros((n,4))
@@ -37,7 +38,7 @@ class finiteMDP:
             if self.absorv[x]:
                 y = x0
             x = y
-        
+
         return J,traj
 
 
@@ -45,44 +46,55 @@ class finiteMDP:
         #nao alterar
         nQ = np.zeros((self.nS,self.nA))
         while True:
-            self.V = np.max(self.Q,axis=1) 
+            self.V = np.max(self.Q,axis=1)
             for a in range(0,self.nA):
                 nQ[:,a] = self.R[:,a] + self.gamma * np.dot(self.P[:,a,:],self.V)
             err = np.linalg.norm(self.Q-nQ)
             self.Q = np.copy(nQ)
             if err<1e-7:
                 break
-            
+
         #update policy
-        self.V = np.max(self.Q,axis=1) 
+        self.V = np.max(self.Q,axis=1)
         #correct for 2 equal actions
         self.Pol = np.argmax(self.Q, axis=1)
-                    
+
         return self.Q,  self.Q2pol(self.Q)
 
-            
+
     def traces2Q(self, trace):
-                # implementar esta funcao
-        
+        auxQ = np.zeros((self.nS,self.nA))
+        alpha = 0.01
+        while True:
+            for step in trace:
+                initial = int(step[0])
+                action = int(step[1])
+                final = int(step[2])
+                reward = step[3]
+                self.Q[initial][action] += alpha * (reward + self.gamma * np.amax(self.Q[final]) - self.Q[initial][action])
+
+            threshold = np.sqrt(sum(sum((self.Q-auxQ)**2)))
+            if threshold < 0.0001:
+                break
+
+            auxQ = copy.deepcopy(self.Q)
 
         return self.Q
-    
+
     def policy(self, x, poltype = 'exploration', par = []):
-        # implementar esta funcao
-        
         if poltype == 'exploitation':
-            pass
+            if par.any():
+                a = np.argmax(par[x])
+            else:
+                a = np.argmax(self.Q[x])
 
-            
         elif poltype == 'exploration':
+            a = np.random.randint(0,self.nA)
+        else:
+            #nao implementado
             pass
 
-                
         return a
-    
+
     def Q2pol(self, Q, eta=5):
-        # implementar esta funcao
         return np.exp(eta*Q)/np.dot(np.exp(eta*Q),np.array([[1,1],[1,1]]))
-
-
-            
